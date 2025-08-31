82b62a973b2e8d7f760ceddeb1d710ffaac24aac
Froshlin <onitimilehin87@gmail>
2025-09-01 00:10:02 +0100

    updated server.js file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://oaustech-enroll-dash.vercel.app/',
+  origin: 'https://oaustech-enroll-dash.vercel.app',
 }));
 app.use(express.json());
 
